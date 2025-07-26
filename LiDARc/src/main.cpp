926f35fc3ea25a181c05cc3bae90fda70dde3fb1
Manav Karonde <karonde.manav@gmail.com>
2025-07-26 12:57:36 -0500

    seems to work across -90 to 90 degrees

--- LiDARc/src/main.cpp
+++ LiDARc/src/main.cpp
@@ -2,23 +2,29 @@
 #include <AccelStepper.h>
 #include <Adafruit_VL53L0X.h>
 
+/*
+2048 steps / revolution
+therefore, 90 deeegrees = 2048 / 4 = 512 steps
+*/
+
+// max steps on left to sweep
+int max_left_angle = -512;
+
+// max steps on right to sweep
+int max_right_angle = 512;
+
 // constants for stepper motor
 #define HALFSTEP 4
-// constant for reading out of range (at arduinos integer max value)
+// constant for reading out of range (makes it -1)
 #define OUT_OF_RANGE 2147483647
 
 // motor and sensor objects
-AccelStepper axis(HALFSTEP, 0, 1, 2, 3);
+AccelStepper axis(HALFSTEP, 9, 11, 10, 12);
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 // num steps motor moved in between readings
-int sweep_angle = 5;
-
-// max angle on left to sweep
-int max_left_angle = -90;
-
-// max angle on right to sweep
-int max_right_angle = 90;
+// for precision, must be factor of max angles
+int sweep_angle = 4;
 
 // current position of the motor
 int current_position = 0;
@@ -32,6 +38,8 @@ void setup() {
   // serial setup
   Serial.begin(115200);
 
+  delay(1000); // give time for serial to initialize
+
   // wait for serial to be ready
   while (!Serial) {
     delay(10); 
@@ -44,8 +52,9 @@ void setup() {
   }
 
   // set motor parameters
-  axis.setMaxSpeed(100);
-  axis.setAcceleration(80);
+  axis.setMaxSpeed(80);
+  axis.setAcceleration(8000);
+  axis.setSpeed(80);
 }
 
 //moved the motor to the next position, if next position is out of bounds reverse direction
@@ -75,6 +84,12 @@ void move_motor()
   {
     axis.moveTo(sweep_angle);
   }
+
+  while(axis.distanceToGo() != 0) {
+    axis.run();
+  }
+
+  current_position = nextPos;
 }
 
 void read_and_proceed()
@@ -83,20 +98,32 @@ void read_and_proceed()
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
 
-  // send reading to serial
-  if (measure.RangeStatus != 4) { // make sure reading in range
-    Serial.println(measure.RangeMilliMeter);
-  } else {
-    Serial.println(OUT_OF_RANGE);
+  int distance = measure.RangeMilliMeter;
+  if (measure.RangeStatus == 4) { // make sure reading in range
+    distance = OUT_OF_RANGE;
   }
+
+  // send reading to serial
+  // output in format: "current_position, distance"
+  Serial.print(current_position);
+  Serial.print(", ");
+  Serial.println(distance);
 
   // move motor to next position
   move_motor();
 }
 
+bool start = true;
+
 void loop()
 {
-  move_motor();
-}
+  if(start)
+  {
+    delay(1000);
+    start = false;
+  }
+  read_and_proceed();
+} 
 
 
+  
