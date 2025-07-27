d84e7fe98bc33e58fcd6a05c29e56bae8f34b826
Manav Karonde <karonde.manav@gmail.com>
2025-07-27 15:17:45 -0500

    added method to oscilate motor

--- LiDARc/src/main.cpp
+++ LiDARc/src/main.cpp
@@ -6,13 +6,16 @@
 #define OUT_OF_RANGE -1     // when sensor sees nothing
 
 // max distance left/right we want the motor to go till
-const int max_angle = 90;
+const int MAX_ANGLE = 90;
+int STEPS_TO_MAX_ANGLE = 0;     // calculate in setup
 
 // motor constants
 const int STEPS_PER_ROTATION = 2048; 
 const int MAX_SPEED = 80;
 const int ACCELERATION = 8000;
-const int STEPS_PER_SCAN = 4; // motor steps in between each scan
+const int STEPS_PER_SCAN = 4;   // motor steps in between each scan
+int current_location = 0;       // current motor angle in the motor's plane (motor ticks)
+bool going_left = true;         // direction of the motor
 
 // Motor / sensor objects
 AccelStepper axis(HALFSTEP, 9, 11, 10, 12);
@@ -48,6 +51,9 @@ void setup()
     Serial.begin(9600);
     delay(1000);
 
+    // will need many times so more effiecient to calculate once
+    STEPS_TO_MAX_ANGLE = ticks_from_degrees(MAX_ANGLE);
+
     // initialize data array
     int num_items = calculate_num_items();
     data = new int[num_items];
@@ -64,4 +70,42 @@ void setup()
     axis.setAcceleration(ACCELERATION);
 }
 
+// runs the motor in the correct direction for one scan
+// updates current location and going left if required
+// returns true if direction changed, false otherwise
+bool move_motor()
+{
+    bool ret = false; // set true if changing direction
 
+    // determine the next position
+    int next_pos = current_location + STEPS_PER_SCAN;
+    if(going_left)
+    {
+        next_pos = current_location - STEPS_PER_SCAN;
+    }        
+
+    // determine if need to switch direction
+    if(next_pos > STEPS_TO_MAX_ANGLE || next_pos < -STEPS_TO_MAX_ANGLE)
+    {
+        going_left = !going_left;
+        next_pos = current_location + (going_left ? -STEPS_PER_SCAN: STEPS_PER_SCAN);
+        ret = true;
+    }
+
+    // move to correct direction
+    axis.setCurrentPosition(0);
+    if(going_left)
+    {
+        axis.moveTo(-STEPS_PER_SCAN);
+    } 
+    else 
+    {
+        axis.moveTo(STEPS_PER_SCAN);
+    }
+
+    while(axis.distanceToGo() != 0)
+    {
+        axis.run();
+    }
+
+}
