a388ceb7c0d2df305dbd143a46ba8536cbcae2bc
Manav Karonde <karonde.manav@gmail.com>
2025-07-27 17:11:40 -0500

    basic plotting on polar map works

--- Interpreter.py
+++ Interpreter.py
@@ -3,24 +3,31 @@ class to read serial data from arduino and convert it to motor steps and distanc
 """
 
 import serial
-
+import time
+from visualizer import PolarVisualizer
 
 class Interpreter:
 
     def __init__(self, baudrate=115200):
-        # code to find the serial port
-        ports = serial.tools.list_ports.comports()
-        port = None
-        for p in ports:
-            if 'Arduino' in p.description or 'ttyUSB' in p.device:
-                port = p.device
-                break   
-        if port is None:
-            raise ValueError("No Arduino found. Please connect an Arduino and try again.")
+        # default serial port, can be overridden
+        port = '/dev/ttyUSB0'  # Change this to your Arduino's
+        
     
         # initialize serial port
         self.serial_port = serial.Serial(port, baudrate)
         self.serial_port.flushInput()
+
+    # method to normalize angle (given in motor ticks) to degrees. The angles is expected between -90 and 90, where -512 would be -90 degrees and 512 would be 90 degrees. There are 2048 ticks per rotation
+    def normalize_angle(self, angle):
+        if not isinstance(angle, (int, float)):
+            raise TypeError("Angle must be an integer or float")
+        if angle < -512 or angle > 512:
+            raise ValueError("Angle must be between -512 and 512")
+        
+        # normalize to degrees
+        # -angle bc angles are inverted in arduino code and easier to fix here
+        normalized_angle = (-angle / 512) * 90
+        return normalized_angle
 
     # method to read a line from serial of the form "angle, distance" and parse to in array of form [angle, distance]
     def parse_line(self):
@@ -28,6 +35,8 @@ class Interpreter:
             line = self.serial_port.readline().decode('utf-8').strip()
             try:
                 angle, distance = map(float, line.split(','))
+                angle = self.normalize_angle(angle)
+                print(f"Received data: Angle = {angle}, Distance = {distance}")
                 return [angle, distance]
             except ValueError:
                 raise ValueError("Invalid data format received from serial port")
@@ -37,3 +46,13 @@ class Interpreter:
     # close serial when done
     def close(self):
         self.serial_port.close()
+
+
+
+time.sleep(5)
+Interpreter = Interpreter()
+polar_visualizer = PolarVisualizer()
+while True:
+    data = Interpreter.parse_line()
+    if data is not None:
+        polar_visualizer.update_point(data[0], data[1])
